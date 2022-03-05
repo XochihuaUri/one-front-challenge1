09e990871be27c1d9d3d0b7a11206d0eb54c7fd7
Uriel Xochihua <urielxochi51@gmail.com>
2022-03-05 16:18:59 -0600

    enlaces

---
+++ script.js
@@ -0,0 +1,17 @@
+const $form = document.querySelector('#form')
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept' : 'aplication/json'
+        }
+    })
+    if(response.ok){
+        alert('Gracias por contactarme, me pondré en contacto contigo.')
+    }
+}
